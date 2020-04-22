14210261d8dc0ce52c7f2b11cdb31ed0c4f20076
michael.ambaye <michaelambaye@Michaels-MBP.lan>
2020-04-22 14:38:56 +0300

    index.js message changed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hello World, this is the first application ever!");
 });
 
 const port = process.env.PORT || 1337;
